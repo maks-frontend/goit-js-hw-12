faadee8f9697c65db54f3cba37238103097f3690
maks-frontend <maksimrazinkov@gmail.com>
2024-04-19 23:53:17 +0300

    hw-12-d

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,26 +1,35 @@
+import axios from 'axios';
+
 const loader = document.querySelector('.loader');
+const loader2 = document.querySelector('.loader2');
 
 export function hideLoader() {
-  loader.classList.add('visually-hidden');
+  loader.classList.toggle('visually-hidden');
 }
 
-export function searchImages(searchInput) {
+export function hideLoader2() {
+  loader2.classList.toggle('visually-hidden');
+}
+
+export async function searchImages(searchInput, page) {
   const API_KEY = '43234755-a337228de2a5121df872aa78d';
-
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: searchInput,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 18,
+  if (page.currentPage === 1) {
+    hideLoader();
+  } else {
+    hideLoader2();
+  }
+  const response = await axios.get('https://pixabay.com/api/', {
+    params: {
+      key: API_KEY,
+      q: searchInput,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page: page.currentPage,
+    },
   });
 
-  loader.classList.remove('visually-hidden');
-  return fetch(`https://pixabay.com/api/?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  page.currentPage += 1;
+  return response;
 }
--- src/main.js
+++ src/main.js
@@ -6,14 +6,25 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 import { searchImages } from './js/pixabay-api.js';
 import { hideLoader } from './js/pixabay-api.js';
+import { hideLoader2 } from './js/pixabay-api.js';
 import { createMarkup } from './js/render-functions.js';
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.searchInput');
 const list = document.querySelector('.list');
 const submitBtn = document.querySelector('.submitBtn');
-
+const loadMore = document.querySelector('.loadMore');
+const page = { currentPage: 1 };
 submitBtn.disabled = true;
+
+function saveToLocalStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+
+function getFromLocalStorage(key) {
+  const data = localStorage.getItem(key);
+  return data ? data : null;
+}
 
 input.addEventListener('input', () => {
   if (input.value.trim() !== '') {
@@ -23,43 +34,110 @@ input.addEventListener('input', () => {
   }
 });
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', handleClick);
+loadMore.addEventListener('click', loadClick);
+
+async function handleClick(event) {
   event.preventDefault();
   const searchInputValue = input.value.trim();
+  page.currentPage = 1;
   if (searchInputValue !== '') {
     list.innerHTML = '';
-    searchImages(searchInputValue)
-      .then(data => {
-        if (data.hits.length === 0) {
-          hideLoader();
-          return iziToast.error({
-            message:
-              'Sorry, there are no images matching your search query. Please try again!',
-            position: 'topRight',
-            backgroundColor: '#EF4040',
-            messageColor: '#fff',
-            timeout: 3000,
-          });
-        } else {
-          hideLoader();
-          list.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-          lightbox.refresh();
-        }
-        form.reset();
-        submitBtn.disabled = true;
-      })
-      .catch(error => {
+    hideLoadMore();
+    saveToLocalStorage('searchInputValue', searchInputValue);
+    try {
+      const response = await searchImages(searchInputValue, page);
+      if (response.data.hits.length === 0) {
         hideLoader();
-        return iziToast.error({
-          message: `${error}`,
-          position: 'topRight',
-          backgroundColor: '#EF4040',
-          messageColor: '#fff',
-          timeout: 3000,
-        });
-      });
+        showErrorToast(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+      } else {
+        hideLoader();
+        list.insertAdjacentHTML('beforeend', createMarkup(response.data.hits));
+        lightbox.refresh();
+        const totalPages = Math.ceil(
+          response.data.totalHits / response.config.params.per_page
+        );
+        checkImageLoad(response, totalPages);
+      }
+      form.reset();
+      submitBtn.disabled = true;
+    } catch (error) {
+      hideLoader();
+      showErrorToast(`${error}`);
+    }
   }
-});
+}
+
+async function loadClick() {
+  hideLoadMore();
+  const searchInputValue = getFromLocalStorage('searchInputValue');
+  try {
+    const response = await searchImages(searchInputValue, page);
+    const totalPages = Math.ceil(
+      response.data.totalHits / response.config.params.per_page
+    );
+    hideLoader2();
+    list.insertAdjacentHTML('beforeend', createMarkup(response.data.hits));
+    lightbox.refresh();
+    scroll();
+    checkImageLoad(response, totalPages);
+  } catch (error) {
+    hideLoader2();
+    showErrorToast(`${error}`);
+  }
+}
+
+function scroll() {
+  const card = list.firstElementChild;
+  const height = card.getBoundingClientRect().height;
+  window.scrollBy({
+    top: height * 2,
+    behavior: 'smooth',
+  });
+}
+
+function showErrorToast(message) {
+  iziToast.error({
+    message: message,
+    position: 'topRight',
+    backgroundColor: '#EF4040',
+    messageColor: '#fff',
+    timeout: 3000,
+  });
+}
+
+function showWarningToast() {
+  iziToast.warning({
+    position: 'topRight',
+    backgroundColor: '#00A36C',
+    message: "We're sorry, but you've reached the end of search results.",
+  });
+}
+
+function showLoadMore() {
+  loadMore.classList.remove('visually-hidden');
+}
+
+function hideLoadMore() {
+  loadMore.classList.add('visually-hidden');
+}
+
+function checkImageLoad(response, totalPages) {
+  const checkImagesInterval = setInterval(() => {
+    const images = list.querySelectorAll('img');
+    const allImagesLoaded = [...images].every(img => img.complete);
+    if (allImagesLoaded) {
+      clearInterval(checkImagesInterval);
+      if (response.config.params.page >= totalPages) {
+        return showWarningToast();
+      } else {
+        showLoadMore();
+      }
+    }
+  }, 50);
+}
 
 const lightbox = new SimpleLightbox('.list a', {
   captionsData: 'alt',
